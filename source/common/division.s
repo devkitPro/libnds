8b856690c6ffc631310afa4688889efb7c6a4621
Dave Murphy <wntrmute@users.sourceforge.net>
2005-06-13 07:07:31 +0000

    sync with ndslib added division overrides to use bios calls

---
+++ source/common/division.s
@@ -0,0 +1,25 @@
+/*
+ *	SWI replacements for the slow libgcc division functions.
+ *
+ *	$Revision: 1.1 $
+ *
+ */
+ 
+	.thumb
+	.align
+	.thumb_func
+	.global	   __divsi3	       /* Signed integer division. */
+	.thumb_set __udivsi3, __divsi3 /* Unsigned integer division. */
+__divsi3:
+	swi 9
+	bx lr
+ 
+	.thumb
+	.align
+	.thumb_func
+	.global	   __modsi3	       /* Signed integer modulus. */
+	.thumb_set __umodsi3, __modsi3 /* Unsigned integer modulus. */
+__modsi3:
+	swi 9
+	mov r0,	r1
+	bx lr
