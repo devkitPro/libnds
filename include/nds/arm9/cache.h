6cde1f693494a9c4d2eaa88f94e08d0ad53215e1
Dave Murphy <wntrmute@users.sourceforge.net>
2005-07-25 16:10:43 +0000

    updated file header

--- include/nds/arm9/cache.h
+++ include/nds/arm9/cache.h
@@ -1,25 +1,31 @@
 /*---------------------------------------------------------------------------------
-  Copyright (C) 2005
-  	Michael Noland (joat)
-  	Jason Rogers (dovoto)
-  	Dave Murphy (WinterMute)
+	$Id: cache.h,v 1.2 2005-07-25 16:10:43 wntrmute Exp $
 
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any
-  damages arising from the use of this software.
+	key input code -- provides slightly higher level input forming
 
-  Permission is granted to anyone to use this software for any
-  purpose, including commercial applications, and to alter it and
-  redistribute it freely, subject to the following restrictions:
+	Copyright (C) 2005
+		Michael Noland (joat)
+		Jason Rogers (dovoto)
+		Dave Murphy (WinterMute)
 
-  1. The origin of this software must not be misrepresented; you
-     must not claim that you wrote the original software. If you use
-     this software in a product, an acknowledgment in the product
-     documentation would be appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and
-     must not be misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source
-     distribution.
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
+
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+	3.	This notice may not be removed or altered from any source
+		distribution.
+
+	$Log: not supported by cvs2svn $
 
 ---------------------------------------------------------------------------------*/
 #ifndef	_cache_h_
