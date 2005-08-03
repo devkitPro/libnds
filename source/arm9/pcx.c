7404bb8d815e6bd37558fbdb2d2d0661dbfeaa75
Dave Murphy <wntrmute@users.sourceforge.net>
2005-08-03 18:08:39 +0000

    *** empty log message ***

--- source/arm9/pcx.c
+++ source/arm9/pcx.c
@@ -1,29 +1,31 @@
 /*---------------------------------------------------------------------------------
-	$Id: pcx.c,v 1.4 2005-08-03 05:18:06 wntrmute Exp $
+	$Id: pcx.c,v 1.5 2005-08-03 18:08:13 wntrmute Exp $
 
+ 	Copyright (C) 2005
+		Jason Rogers (dovoto)
+		Dave Murphy (WinterMute)
 
-  Copyright (C) 2005
-			Jason Rogers (dovoto)
-			Dave Murphy (WinterMute)
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
 
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any
-  damages arising from the use of this software.
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
 
-  Permission is granted to anyone to use this software for any
-  purpose, including commercial applications, and to alter it and
-  redistribute it freely, subject to the following restrictions:
-
-  1. The origin of this software must not be misrepresented; you
-     must not claim that you wrote the original software. If you use
-     this software in a product, an acknowledgment in the product
-     documentation would be appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and
-     must not be misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source
-     distribution.
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+	3.	This notice may not be removed or altered from any source
+		distribution.
 
 	$Log: not supported by cvs2svn $
+	Revision 1.4  2005/08/03 05:18:06  wntrmute
+	tidied formatting
+	
 	Revision 1.3  2005/08/03 05:05:59  dovoto
 	added rounding to palette conversion
 	
