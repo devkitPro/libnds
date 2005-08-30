a1477c7e60b771b78817c08a028feb0e10ca45a3
Dave Murphy <wntrmute@users.sourceforge.net>
2005-08-30 17:52:24 +0000

    corrected CARD_CR1 fixed cardReadEeprom

--- include/nds/card.h
+++ include/nds/card.h
@@ -1,45 +1,40 @@
-//////////////////////////////////////////////////////////////////////
-//
-// card.h -- DS Memory card registers and accessors
-//
-// version 0.1, February 14, 2005
-//
-//  Copyright (C) 2005 Michael Noland (joat) and Jason Rogers (dovoto)
-//
-//  This software is provided 'as-is', without any express or implied
-//  warranty.  In no event will the authors be held liable for any
-//  damages arising from the use of this software.
-//
-//  Permission is granted to anyone to use this software for any
-//  purpose, including commercial applications, and to alter it and
-//  redistribute it freely, subject to the following restrictions:
-//
-//  1. The origin of this software must not be misrepresented; you
-//     must not claim that you wrote the original software. If you use
-//     this software in a product, an acknowledgment in the product
-//     documentation would be appreciated but is not required.
-//  2. Altered source versions must be plainly marked as such, and
-//     must not be misrepresented as being the original software.
-//  3. This notice may not be removed or altered from any source
-//     distribution.
-//
-// Changelog:
-//   0.1: First version
-//
-//////////////////////////////////////////////////////////////////////
+/*---------------------------------------------------------------------------------
+	$Id: card.h,v 1.2 2005-08-30 17:52:24 wntrmute Exp $
+
+	Copyright (C) 2005
+		Michael Noland (joat)
+		Jason Rogers (dovoto)
+		Dave Murphy (WinterMute)
+
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
+
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+	3.	This notice may not be removed or altered from any source
+		distribution.
+
+	$Log: not supported by cvs2svn $
+
+---------------------------------------------------------------------------------*/
 
 #ifndef NDS_CARD_INCLUDE
 #define NDS_CARD_INCLUDE
 
-//////////////////////////////////////////////////////////////////////
 
 #include "jtypes.h"
 
-//////////////////////////////////////////////////////////////////////
 // Card bus
-//////////////////////////////////////////////////////////////////////
-
-#define CARD_CR1       (*(vuint32*)0x040001A0)
+#define CARD_CR1       (*(vuint16*)0x040001A0)
 #define CARD_CR1H      (*(vuint8*)0x040001A1)
 #define CARD_EEPDATA   (*(vuint8*)0x040001A2)
 #define CARD_CR2       (*(vuint32*)0x040001A4)
@@ -53,12 +48,10 @@
 #define CARD_1B8       (*(vuint16*)0x040001B8)
 #define CARD_1BA       (*(vuint16*)0x040001BA)
 
-//////////////////////////////////////////////////////////////////////
 
 #define CARD_CR1_ENABLE  0x80  // in byte 1, i.e. 0x8000
 #define CARD_CR1_IRQ     0x40  // in byte 1, i.e. 0x4000
 
-//////////////////////////////////////////////////////////////////////
 
 // CARD_CR2 register:
 
@@ -79,13 +72,11 @@
 #define CARD_BUSY       (1<<31)  // when reading, still expecting incomming data?
 #define CARD_DATA_READY (1<<23)  // when reading, CARD_DATA_RD or CARD_DATA has another word of data and is good to go
 
-//////////////////////////////////////////////////////////////////////
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-//////////////////////////////////////////////////////////////////////
 
 void cardWriteCommand(uint8 * command);
 
@@ -99,14 +90,11 @@ int cardReadID(uint32 flags);
 void cardReadEeprom(uint32 address, uint8 *data, uint32 length, uint32 addrtype);
 void cardWriteEeprom(uint32 address, uint8 *data, uint32 length, uint32 addrtype);
 
-//////////////////////////////////////////////////////////////////////
 
 #ifdef __cplusplus
 }
 #endif
 
-//////////////////////////////////////////////////////////////////////
 
 #endif
 
-//////////////////////////////////////////////////////////////////////
