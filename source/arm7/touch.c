01feb93b6a0db3020597f40f852d733ecad2a4d5
Dave Murphy <wntrmute@users.sourceforge.net>
2005-09-07 18:05:37 +0000

    use macros for device settings

--- source/arm7/touch.c
+++ source/arm7/touch.c
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------
-	$Id: touch.c,v 1.6 2005-08-23 17:06:10 wntrmute Exp $
+	$Id: touch.c,v 1.7 2005-09-07 18:05:37 wntrmute Exp $
 
 	Touch screen control for the ARM7
 
@@ -26,70 +26,62 @@
 			distribution.
 
 	$Log: not supported by cvs2svn $
+	Revision 1.6  2005/08/23 17:06:10  wntrmute
+	converted all endings to unix
+
 	Revision 1.5  2005/08/01 23:12:17  wntrmute
 	extended touchReadXY to return touchscreen co-ordinates as well as screen co-ordinates
-	
+
 	Revision 1.4  2005/07/29 00:57:40  wntrmute
 	updated file headers
 	added touchReadXY function
 	made header C++ compatible
-	
+
 	Revision 1.3  2005/07/12 17:32:20  wntrmute
 	updated file header
-	
+
 	Revision 1.2  2005/07/11 23:12:15  wntrmute
 	*** empty log message ***
 
 ---------------------------------------------------------------------------------*/
 
-#include "nds/arm7/touch.h"
-
-
-// these are pixel positions of the two points you click when calibrating
-#define TOUCH_CNTRL_X1   (*(vu8*)0x027FFCDC)
-#define TOUCH_CNTRL_Y1   (*(vu8*)0x027FFCDD)
-#define TOUCH_CNTRL_X2   (*(vu8*)0x027FFCE2)
-#define TOUCH_CNTRL_Y2   (*(vu8*)0x027FFCE3)
-
-// these are the corresponding touchscreen values:
-#define TOUCH_CAL_X1   (*(vu16*)0x027FFCD8)
-#define TOUCH_CAL_Y1   (*(vu16*)0x027FFCDA)
-#define TOUCH_CAL_X2   (*(vu16*)0x027FFCDE)
-#define TOUCH_CAL_Y2   (*(vu16*)0x027FFCE0)
+#include <nds/jtypes.h>
+#include <nds/system.h>
+#include <nds/arm7/touch.h>
 
 
 //---------------------------------------------------------------------------------
 uint16 touchRead(uint32 command) {
 //---------------------------------------------------------------------------------
-  uint16 result;
-  SerialWaitBusy();
+	uint16 result;
+	SerialWaitBusy();
 
-  // Write the command and wait for it to complete
-  SERIAL_CR = SERIAL_ENABLE | 0xA01;
-  SERIAL_DATA = command;
-  SerialWaitBusy();
+	// Write the command and wait for it to complete
+	REG_SPICNT = SPI_ENABLE | SPI_BAUD_2MHz | SPI_DEVICE_TOUCH | SPI_CONTINUOUS; //0x0A01;
+	REG_SPIDATA = command;
+	SerialWaitBusy();
 
-  // Write the second command and clock in part of the data
-  SERIAL_DATA = 0;
-  SerialWaitBusy();
-  result = SERIAL_DATA;
+	// Write the second command and clock in part of the data
+	REG_SPIDATA = 0;
+	SerialWaitBusy();
+	result = REG_SPIDATA;
 
-  // Clock in the rest of the data (last transfer)
-  SERIAL_CR = SERIAL_ENABLE | 0x201;
-  SERIAL_DATA = 0;
-  SerialWaitBusy();
+	// Clock in the rest of the data (last transfer)
+	REG_SPICNT = SPI_ENABLE | 0x201;
+	REG_SPIDATA = 0;
+	SerialWaitBusy();
 
-  // Return the result
-  return ((result & 0x7F) << 5) | (SERIAL_DATA >> 3);
+	// Return the result
+	return ((result & 0x7F) << 5) | (REG_SPIDATA >> 3);
 }
 
 
 //---------------------------------------------------------------------------------
 uint32 touchReadTemperature(int * t1, int * t2) {
 //---------------------------------------------------------------------------------
-  *t1 = touchRead(TSC_MEASURE_TEMP1);
-  *t2 = touchRead(TSC_MEASURE_TEMP2);
-  return 8490 * (*t2 - *t1) - 273*4096;
+	*t1 = touchRead(TSC_MEASURE_TEMP1);
+	*t2 = touchRead(TSC_MEASURE_TEMP2);
+	return 8490 * (*t2 - *t1) - 273*4096;
 }
 
 
@@ -98,37 +90,36 @@ uint32 touchReadTemperature(int * t1, int * t2) {
 // linear mapping can be used to go from touchscreen position to pixel position
 
 static bool touchInit = false;
+static u32 xscale, yscale;
+static s32 xoffset, yoffset;
 
 // reading pixel position:
 //---------------------------------------------------------------------------------
 touchPosition touchReadXY() {
 //---------------------------------------------------------------------------------
 
-	
+
 	touchPosition touchPos;
 
-	static int TOUCH_WIDTH;
-	static int TOUCH_HEIGHT;
-	static int CNTRL_WIDTH;
-	static int CNTRL_HEIGHT;
-	
+
 	if ( !touchInit ) {
 
-		TOUCH_WIDTH  = TOUCH_CAL_X2 - TOUCH_CAL_X1;
-		TOUCH_HEIGHT = TOUCH_CAL_Y2 - TOUCH_CAL_Y1;
-		CNTRL_WIDTH  = TOUCH_CNTRL_X2 - TOUCH_CNTRL_X1;
-		CNTRL_HEIGHT = TOUCH_CNTRL_Y2 - TOUCH_CNTRL_Y1;
 
-		
+		xscale = ((PersonalData->calX2px - PersonalData->calX1px) << 16) / (PersonalData->calX2 - PersonalData->calX1);
+		yscale = ((PersonalData->calY2px - PersonalData->calY1px) << 16) / (PersonalData->calY2 - PersonalData->calY1);
+
+		xoffset = PersonalData->calX1 * xscale - (PersonalData->calX1px << 16);
+		yoffset = PersonalData->calY1 * yscale - (PersonalData->calY1px << 16);
+
 		touchInit = true;
 
 	}
-	
+
 	touchPos.x = touchRead(TSC_MEASURE_X);
 	touchPos.y = touchRead(TSC_MEASURE_Y);
 
-	touchPos.px = ( touchPos.x - (int16) TOUCH_CAL_X1) * CNTRL_WIDTH  / TOUCH_WIDTH + (int16) TOUCH_CNTRL_X1;
-	touchPos.py = ( touchPos.y - (int16) TOUCH_CAL_Y1) * CNTRL_HEIGHT / TOUCH_HEIGHT + (int16) TOUCH_CNTRL_Y1;
+	touchPos.px = ( touchPos.x * xscale - xoffset ) >>16;
+	touchPos.py = ( touchPos.y * yscale - yoffset ) >>16;
 
 	return touchPos;
 
