0e014cf119062d825f88f6c8303d1cf8eaa428eb
Dave Murphy <wntrmute@users.sourceforge.net>
2005-09-20 05:05:52 +0000

    added header logging tidied formatting

--- source/arm7/clock.c
+++ source/arm7/clock.c
@@ -1,37 +1,36 @@
-//////////////////////////////////////////////////////////////////////
-//
-// clock.cpp -- Real Time Clock accessor functions for the ARM7
-//
-// version 0.1, February 14, 2005
-//
-//  Copyright (C) 2005 Michael Noland (joat) and Jason Rogers (dovoto)
-//
-//  This software is provided 'as-is', without any express or implied
-//  warranty.  In no event will the authors be held liable for any
-//  damages arising from the use of this software.
-//
-//  Permission is granted to anyone to use this software for any
-//  purpose, including commercial applications, and to alter it and
-//  redistribute it freely, subject to the following restrictions:
-//
-//  1. The origin of this software must not be misrepresented; you
-//     must not claim that you wrote the original software. If you use
-//     this software in a product, an acknowledgment in the product
-//     documentation would be appreciated but is not required.
-//  2. Altered source versions must be plainly marked as such, and
-//     must not be misrepresented as being the original software.
-//  3. This notice may not be removed or altered from any source
-//     distribution.
-//
-// Changelog:
-//   0.1: First version
-//
-//////////////////////////////////////////////////////////////////////
+/*---------------------------------------------------------------------------------
+	$Id: clock.c,v 1.3 2005-09-20 05:05:52 wntrmute Exp $
+
+
+	Copyright (C) 2005
+		Michael Noland (Joat)
+		Jason Rogers (Dovoto)
+		Dave Murphy (WinterMute)
+
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
+
+	1.	The origin of this software must not be misrepresented; you
+			must not claim that you wrote the original software. If you use
+			this software in a product, an acknowledgment in the product
+			documentation would be appreciated but is not required.
+	2.	Altered source versions must be plainly marked as such, and
+			must not be misrepresented as being the original software.
+	3.	This notice may not be removed or altered from any source
+			distribution.
+
+	$Log: not supported by cvs2svn $
+
+---------------------------------------------------------------------------------*/
 
 #include "nds/bios.h"
 #include "nds/arm7/clock.h"
 
-//////////////////////////////////////////////////////////////////////
 
 // Delay (in swiDelay units) for each bit transfer
 #define RTC_DELAY 48
@@ -46,113 +45,120 @@
 #define SIO_out (1<<4)
 #define SIO_in  (1)
 
-//////////////////////////////////////////////////////////////////////
 
+//---------------------------------------------------------------------------------
 void rtcTransaction(uint8 * command, uint32 commandLength, uint8 * result, uint32 resultLength) {
-  uint32 bit;
-  uint8 data;
+//---------------------------------------------------------------------------------
+	uint32 bit;
+	uint8 data;
 
-  // Validate the parameters
+// Validate the parameters
 //  ASSERT(( (!(*command & 1)) && (!resultLength) && (commandLength > 0) )    // Valid write
-  //    || ( (*command & 1) && (resultLength > 0) && (commandLength == 1)) ); // Vaild read
+//    || ( (*command & 1) && (resultLength > 0) && (commandLength == 1)) ); // Vaild read
 
-  // Raise CS
-  RTC_CR8 = CS_0 | SCK_1 | SIO_1;
-  swiDelay(RTC_DELAY);
-  RTC_CR8 = CS_1 | SCK_1 | SIO_1;
-  swiDelay(RTC_DELAY);
+	// Raise CS
+	RTC_CR8 = CS_0 | SCK_1 | SIO_1;
+	swiDelay(RTC_DELAY);
+	RTC_CR8 = CS_1 | SCK_1 | SIO_1;
+	swiDelay(RTC_DELAY);
 
-  // Write command bytes (high bit first)
-  for ( ; commandLength > 0; commandLength--) {
-    data = *command++;
-    for (bit = 0; bit < 8; bit++) {
-      RTC_CR8 = CS_1 | SCK_0 | SIO_out | (data>>7);
-      swiDelay(RTC_DELAY);
+	// Write command bytes (high bit first)
+	for ( ; commandLength > 0; commandLength--) {
+		data = *command++;
 
-      RTC_CR8 = CS_1 | SCK_1 | SIO_out | (data>>7);
-      swiDelay(RTC_DELAY);
+		for (bit = 0; bit < 8; bit++) {
+			RTC_CR8 = CS_1 | SCK_0 | SIO_out | (data>>7);
+			swiDelay(RTC_DELAY);
 
-      data = data << 1;
-    }
-  }
+			RTC_CR8 = CS_1 | SCK_1 | SIO_out | (data>>7);
+			swiDelay(RTC_DELAY);
 
-  // Read result bytes (low bit first)
-  for ( ; resultLength > 0; resultLength--) {
-    data = 0;
-    for (bit = 0; bit < 8; bit++) {
-      RTC_CR8 = CS_1 | SCK_0;
-      swiDelay(RTC_DELAY);
+			data = data << 1;
+		}
+	}
 
-      RTC_CR8 = CS_1 | SCK_1;
-      swiDelay(RTC_DELAY);
+	// Read result bytes (low bit first)
+	for ( ; resultLength > 0; resultLength--) {
+		data = 0;
 
-      if (RTC_CR8 & SIO_in) data |= (1 << bit);
-    }
-    *result++ = data;
-  }
+		for (bit = 0; bit < 8; bit++) {
+			RTC_CR8 = CS_1 | SCK_0;
+			swiDelay(RTC_DELAY);
 
-  // Finish up by dropping CS low
-  RTC_CR8 = CS_0 | SCK_1;
-  swiDelay(RTC_DELAY);
+			RTC_CR8 = CS_1 | SCK_1;
+			swiDelay(RTC_DELAY);
+
+			if (RTC_CR8 & SIO_in) data |= (1 << bit);
+		}
+		*result++ = data;
+	}
+
+	// Finish up by dropping CS low
+	RTC_CR8 = CS_0 | SCK_1;
+	swiDelay(RTC_DELAY);
 }
 
-//////////////////////////////////////////////////////////////////////
 
+//---------------------------------------------------------------------------------
 void rtcReset(void) {
-  uint8 status;
-  uint8 command[2];
+//---------------------------------------------------------------------------------
+	uint8 status;
+	uint8 command[2];
 
-  // Read the first status register
-  command[0] = READ_STATUS_REG1;
-  rtcTransaction(command, 1, &status, 1);
+	// Read the first status register
+	command[0] = READ_STATUS_REG1;
+	rtcTransaction(command, 1, &status, 1);
 
-  // Reset the RTC if needed
-  if (status & (STATUS_POC | STATUS_BLD)) {
-    command[0] = WRITE_STATUS_REG1;
-    command[1] = status | STATUS_RESET;
-    rtcTransaction(command, 2, 0, 0);
-  }
+	// Reset the RTC if needed
+	if (status & (STATUS_POC | STATUS_BLD)) {
+		command[0] = WRITE_STATUS_REG1;
+		command[1] = status | STATUS_RESET;
+		rtcTransaction(command, 2, 0, 0);
+	}
 }
 
-//////////////////////////////////////////////////////////////////////
 
+//---------------------------------------------------------------------------------
 void rtcGetTime(uint8 * time) {
-  uint8 command, status;
+//---------------------------------------------------------------------------------
+	uint8 command, status;
 
-  time[0] = READ_DATA_REG1;
-  rtcTransaction(&(time[0]), 1, &(time[1]), 7);
+	time[0] = READ_DATA_REG1;
+	rtcTransaction(&(time[0]), 1, &(time[1]), 7);
 
-  command = READ_STATUS_REG1;
-  rtcTransaction(&command, 1, &status, 1);
-  time[0] = status;
+	command = READ_STATUS_REG1;
+	rtcTransaction(&command, 1, &status, 1);
+	time[0] = status;
 }
 
-//////////////////////////////////////////////////////////////////////
 
+//---------------------------------------------------------------------------------
 void rtcSetTime(uint8 * time) {
-  time[0] = WRITE_DATA_REG1;
-  // fixme: range checking on the data we tell it
-  rtcTransaction(time, 8, 0, 0);
+//---------------------------------------------------------------------------------
+	time[0] = WRITE_DATA_REG1;
+	// fixme: range checking on the data we tell it
+	rtcTransaction(time, 8, 0, 0);
 }
 
-//////////////////////////////////////////////////////////////////////
 
+//---------------------------------------------------------------------------------
 void BCDToInteger(uint8 * data, uint32 length) {
-  u32 i;
+//---------------------------------------------------------------------------------
+	u32 i;
 	for (i = 0; i < length; i++) {
-    data[i] = (data[i] & 0xF) + ((data[i] & 0xF0)>>4)*10;
-  }
+		data[i] = (data[i] & 0xF) + ((data[i] & 0xF0)>>4)*10;
+	}
 }
 
-//////////////////////////////////////////////////////////////////////
 
+//---------------------------------------------------------------------------------
 void integerToBCD(uint8 * data, uint32 length) {
-  u32 i;
+//---------------------------------------------------------------------------------
+	u32 i;
 	for (i = 0; i < length; i++) {
-    int high, low;
-    swiDivMod(data[i], 10, &high, &low);
-    data[i] = (high<<4) | low;
-  }
+		int high, low;
+		swiDivMod(data[i], 10, &high, &low);
+		data[i] = (high<<4) | low;
+	}
 }
 
-//////////////////////////////////////////////////////////////////////
