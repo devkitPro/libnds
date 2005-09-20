a18f6207240ba3d0c87adb04e436197f24acefbd
Dave Murphy <wntrmute@users.sourceforge.net>
2005-09-20 04:59:04 +0000

    replaced defines with enums replaced macros with static inlines

--- include/nds/ipc.h
+++ include/nds/ipc.h
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------
-	$Id: ipc.h,v 1.11 2005-08-23 17:06:10 wntrmute Exp $
+	$Id: ipc.h,v 1.12 2005-09-20 04:59:04 wntrmute Exp $
 
 	Inter Processor Communication
 
@@ -26,14 +26,17 @@
 		distribution.
 
 	$Log: not supported by cvs2svn $
+	Revision 1.11  2005/08/23 17:06:10  wntrmute
+	converted all endings to unix
+
 	Revision 1.10  2005/08/03 05:26:42  wntrmute
 	use BIT macro
 	corrected header include
-	
+
 	Revision 1.9  2005/07/27 02:20:05  wntrmute
 	resynchronise with ndslib
 	Updated GL with float wrappers for NeHe
-	
+
 
 ---------------------------------------------------------------------------------*/
 
@@ -55,8 +58,8 @@ typedef struct sTransferSoundData {
   u8 format;
   u8 PADDING;
 } TransferSoundData, * pTransferSoundData;
- 
- 
+
+
 //---------------------------------------------------------------------------------
 typedef struct sTransferSound {
 //---------------------------------------------------------------------------------
@@ -64,42 +67,42 @@ typedef struct sTransferSound {
   u8 count;
   u8 PADDING[3];
 } TransferSound, * pTransferSound;
- 
+
 
 //---------------------------------------------------------------------------------
 typedef struct sTransferRegion {
 //---------------------------------------------------------------------------------
   uint32 heartbeat;          // counts frames
- 
+
    int16 touchX,   touchY;   // TSC X, Y
    int16 touchXpx, touchYpx; // TSC X, Y pixel values
    int16 touchZ1,  touchZ2;  // TSC x-panel measurements
   uint16 tdiode1,  tdiode2;  // TSC temperature diodes
   uint32 temperature;        // TSC computed temperature
- 
+
   uint16 buttons;            // X, Y, /PENIRQ buttons
- 
+
   union {
     uint8 curtime[8];        // current time response from RTC
- 
+
     struct {
       u8 rtc_command;
       u8 rtc_year;           //add 2000 to get 4 digit year
       u8 rtc_month;          //1 to 12
       u8 rtc_day;            //1 to (days in month)
- 
+
       u8 rtc_incr;
       u8 rtc_hours;          //0 to 11 for AM, 52 to 63 for PM
       u8 rtc_minutes;        //0 to 59
       u8 rtc_seconds;        //0 to 59
     };
   };
- 
+
   uint16 battery;            // battery life ??  hopefully.  :)
   uint16 aux;                // i have no idea...
- 
+
   pTransferSound soundData;
- 
+
   // Don't rely on these below, will change or be removed in the future
   vuint32 mailAddr;
   vuint32 mailData;
@@ -117,33 +120,45 @@ typedef struct sTransferRegion {
 #define IPC_LID_CLOSED BIT(7)
 
 //---------------------------------------------------------------------------------
-// Synchronization register 
+// Synchronization register
 //---------------------------------------------------------------------------------
-#define IPC_SYNC				(*(vuint16*)0x04000180)
+#define REG_IPC_SYNC	(*(vuint16*)0x04000180)
 
-#define IPC_SYNC_IRQ_ENABLE		BIT(14)
-#define IPC_SYNC_IRQ_REQUEST	BIT(13)
+enum IPC_SYNC_BITS {
+	IPC_SYNC_IRQ_ENABLE		=	BIT(14),
+	IPC_SYNC_IRQ_REQUEST	=	BIT(13)
+};
 
-#define IPC_SYNC_SEND_COMMAND(n)	(IPC_SYNC = (IPC_SYNC & 0xF0FF) | (((n) & 0xF) << 8) | IPC_SYNC_IRQ_REQUEST)
-#define IPC_SYNC_GET_COMMAND		((IPC_SYNC & 0xF) )
+//---------------------------------------------------------------------------------
+static inline void IPC_SendSync(unsigned int sync) {
+//---------------------------------------------------------------------------------
+	REG_IPC_SYNC = (REG_IPC_SYNC & 0xf0ff) | (((sync) & 0x0f) << 8) | IPC_SYNC_IRQ_REQUEST;
+}
+
+//---------------------------------------------------------------------------------
+static inline int IPC_GetSync() {
+//---------------------------------------------------------------------------------
+	return REG_IPC_SYNC & 0x0f;
+}
 
 //---------------------------------------------------------------------------------
 // fifo
 //---------------------------------------------------------------------------------
-#define IPC_FIFO_SEND              (*(vu32*)0x4000188)                   
-#define IPC_FIFO_RECIEVE           (*(vu32*)0x4100000)                                      
-#define IPC_FIFO_CR                (*(vu16*)0x4000184)
+#define REG_IPC_FIFO_TX		(*(vu32*)0x4000188)
+#define REG_IPC_FIFO_RX		(*(vu32*)0x4100000)
+#define REG_IPC_FIFO_CR		(*(vu16*)0x4000184)
 
-#define IPC_FIFO_SEND_EMPTY (1<<0)
-#define IPC_FIFO_SEND_FULL  (1<<1)
-#define IPC_FIFO_SEND_IRQ   (1<<2)
-#define IPC_FIFO_SEND_CLEAR (1<<3)
-#define IPC_FIFO_RECV_EMPTY (1<<8)
-#define IPC_FIFO_RECV_FULL  (1<<9)
-#define IPC_FIFO_RECV_IRQ   (1<<10)
-#define IPC_FIFO_ERROR      (1<<14)
-#define IPC_FIFO_ENABLE     (1<<15)
-
+enum IPC_CONTROL_BITS {
+	IPC_FIFO_SEND_EMPTY	=	(1<<0),
+	IPC_FIFO_SEND_FULL	=	(1<<1),
+	IPC_FIFO_SEND_IRQ	=	(1<<2),
+	IPC_FIFO_SEND_CLEAR	=	(1<<3),
+	IPC_FIFO_RECV_EMPTY	=	(1<<8),
+	IPC_FIFO_RECV_FULL	=	(1<<9),
+	IPC_FIFO_RECV_IRQ	=	(1<<10),
+	IPC_FIFO_ERROR		=	(1<<14),
+	IPC_FIFO_ENABLE		=	(1<<15)
+};
 
 #endif
 
