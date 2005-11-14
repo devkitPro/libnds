a99784d35bb625b448646f50348fc6094dedbccc
Dave Murphy <wntrmute@users.sourceforge.net>
2005-11-14 11:59:49 +0000

    reformat for consistency

--- include/nds/arm9/input.h
+++ include/nds/arm9/input.h
@@ -1,32 +1,35 @@
 /*---------------------------------------------------------------------------------
-	$Id: input.h,v 1.7 2005-11-03 23:38:49 wntrmute Exp $
+	$Id: input.h,v 1.8 2005-11-14 11:59:49 wntrmute Exp $
 
 	key input code -- provides slightly higher level input forming
 
-  Copyright (C) 2005
-			Michael Noland (joat)
-			Jason Rogers (dovoto)
-			Christian Auby (DesktopMan)
-			Dave Murphy (WinterMute)
+	Copyright (C) 2005
+		Michael Noland (joat)
+		Jason Rogers (dovoto)
+		Christian Auby (DesktopMan)
+		Dave Murphy (WinterMute)
 
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any
-  damages arising from the use of this software.
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
 
-  Permission is granted to anyone to use this software for any
-  purpose, including commercial applications, and to alter it and
-  redistribute it freely, subject to the following restrictions:
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
 
-  1. The origin of this software must not be misrepresented; you
-     must not claim that you wrote the original software. If you use
-     this software in a product, an acknowledgment in the product
-     documentation would be appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and
-     must not be misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source
-     distribution.
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+	3.	This notice may not be removed or altered from any source
+		distribution.
 
 	$Log: not supported by cvs2svn $
+	Revision 1.7  2005/11/03 23:38:49  wntrmute
+	don't use enum for key function returns
+	
 	Revision 1.6  2005/10/18 04:17:04  wntrmute
 	doxygenation
 	
@@ -115,8 +118,6 @@ extern "C" {
 /*! \fn scanKeys()
 
 	\brief obtain the current keypad states.
-
-
 
 	Call this function once per main loop in order to use the keypad functions.
 
