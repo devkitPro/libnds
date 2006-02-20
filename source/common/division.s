9292ae6c40bf2d69fdd54a265430c3bb1cfbae9d
Raleigh Cross <crossraleigh@users.sourceforge.net>
2006-02-20 18:00:42 +0000

    Fixed sign issue for division override

--- source/common/division.s
+++ source/common/division.s
@@ -1,24 +1,31 @@
 /*
  *	SWI replacements for the slow libgcc division functions.
  *
- *	$Revision: 1.2 $
+ *	$Revision: 1.3 $
  *
  */
- 
+
 	.thumb
 	.align
 	.thumb_func
 	.global	   __divsi3	       /* Signed integer division. */
-	.thumb_set __udivsi3, __divsi3 /* Unsigned integer division. */
 __divsi3:
 	swi 9
 	bx lr
- 
+
+	.thumb
+	.align
+	.thumb_func
+	.global	   __udivsi3	       /* Unsigned integer division. */
+__udivsi3:
+	swi 9
+        mov r0, r3
+	bx lr
+
 	.thumb
 	.align
 	.thumb_func
 	.global	   __modsi3	       /* Signed integer modulus. */
-	.thumb_set __umodsi3, __modsi3 /* Unsigned integer modulus. */
 __modsi3:
 	swi 9
 	mov r0,	r1
