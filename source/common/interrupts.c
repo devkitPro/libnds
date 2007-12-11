f959258b2e37af19d61161ee07b481c0a59796b9
Dave Murphy <wntrmute@users.sourceforge.net>
2007-12-11 11:16:07 +0000

    remove header logging consolidate irqDisable & irqClear

--- source/common/interrupts.c
+++ source/common/interrupts.c
@@ -1,5 +1,4 @@
 /*---------------------------------------------------------------------------------
-	$Id: interrupts.c,v 1.10 2007-02-06 12:00:32 wntrmute Exp $
 
 	Copyright (C) 2005
 		Dave Murphy (WinterMute)
@@ -20,32 +19,6 @@
 		must not be misrepresented as being the original software.
 	3.	This notice may not be removed or altered from any source
 		distribution.
-
-	$Log: not supported by cvs2svn $
-	Revision 1.9  2006/06/16 07:35:14  wntrmute
-	more moves to REG_ values
-	implement SetYtrigger helper function
-	set bits in REG_DISPSTAT for IRQ_VCOUNT
-	
-	Revision 1.8  2006/02/21 00:12:35  wntrmute
-	clear interrupts and enable REG_IME in irqInit
-	
-	Revision 1.7  2005/12/12 13:03:29  wntrmute
-	set/clear LCD interrupts in irqEnable/Disable
-	
-	Revision 1.6  2005/11/07 01:11:08  wntrmute
-	more doxygen
-	moved IntrMain declaration to C file
-	
-	Revision 1.5  2005/09/20 05:00:24  wntrmute
-	use new register definitions
-	
-	Revision 1.4  2005/09/04 16:28:04  wntrmute
-	place irqTable in ewram
-
-	Revision 1.3  2005/09/03 17:09:35  wntrmute
-	added interworking aware interrupt dispatcher
-
 
 ---------------------------------------------------------------------------------*/
 
@@ -86,8 +59,6 @@ void irqSet(int mask, IntFn handler) {
 		REG_DISPSTAT |= DISP_VBLANK_IRQ ;
 	if(mask & IRQ_HBLANK)
 		REG_DISPSTAT |= DISP_HBLANK_IRQ ;
-
-	REG_IE |= mask;
 }
 
 //---------------------------------------------------------------------------------
@@ -108,29 +79,6 @@ void irqInit() {
 	REG_IF	= IRQ_ALL;		// clear all pending interrupts
 	REG_IME = 1;			// enable global interrupt
 
-}
-
-
-//---------------------------------------------------------------------------------
-void irqClear(int mask) {
-//---------------------------------------------------------------------------------
-	int i = 0;
-
-	for	(i=0;i<MAX_INTERRUPTS;i++)
-		if	(irqTable[i].mask == mask) break;
-
-	if ( i == MAX_INTERRUPTS ) return;
-
-	irqTable[i].handler	= irqDummy;
-
-	if (mask & IRQ_VBLANK)
-		REG_DISPSTAT &= ~DISP_VBLANK_IRQ ;
-	if (mask & IRQ_HBLANK)
-		REG_DISPSTAT &= ~DISP_HBLANK_IRQ ;
-	if (mask & IRQ_VCOUNT)
-		REG_DISPSTAT &= ~DISP_YTRIGGER_IRQ;
-
-	REG_IE &= ~mask;
 }
 
 
@@ -173,3 +121,17 @@ void irqDisable(uint32 irq) {
 	REG_IE &= ~irq;
 }
 
+//---------------------------------------------------------------------------------
+void irqClear(int mask) {
+//---------------------------------------------------------------------------------
+	int i = 0;
+
+	for	(i=0;i<MAX_INTERRUPTS;i++)
+		if	(irqTable[i].mask == mask) break;
+
+	if ( i == MAX_INTERRUPTS ) return;
+
+	irqDisable( mask);
+
+	irqTable[i].handler	= irqDummy;
+}
