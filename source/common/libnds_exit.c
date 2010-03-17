da6adfbb9b52f420be7ceed15915814192e66be9
Dave Murphy <wntrmute@users.sourceforge.net>
2010-03-17 10:25:50 +0000

    add hook for non zero exit

--- source/common/libnds_exit.c
+++ source/common/libnds_exit.c
@@ -3,8 +3,15 @@
 
 extern char *fake_heap_end;
 
+void __attribute__((weak)) systemErrorExit(int rc) {
+	
+}
+
 void __libnds_exit(int rc) {
 
+	if (rc != 0) {
+		systemErrorExit(rc);
+	}
 	struct __bootstub *bootcode = __transferRegion()->bootcode;
 
 	if (bootcode->bootsig == BOOTSIG) {
