49c2785091a8c5ea0f38210eb93f1db7660c9cf4
Dave Murphy <wntrmute@users.sourceforge.net>
2011-02-01 03:49:25 +0000

    add i2c code

--- include/nds.h
+++ include/nds.h
@@ -280,6 +280,7 @@ extern "C" {
 #include "nds/arm7/audio.h"
 #include "nds/arm7/clock.h"
 #include "nds/arm7/input.h"
+#include "nds/arm7/i2c.h"
 #include "nds/arm7/serial.h"
 #include "nds/arm7/touch.h"
 
